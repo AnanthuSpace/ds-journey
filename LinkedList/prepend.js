7bdeae746b80feb052fcf083fdd3786808c1d04a
Ananthu Mohan <ananthumohan368@gmail.com>
2024-03-31 21:56:57 +0530

    prepend if list is empty or existing list

---
+++ LinkedList/prepend.js
@@ -0,0 +1,41 @@
+class Node {
+    constructor(value){
+        this.value = value
+        this.next  = null
+    }
+}
+
+class linkedList{
+    constructor(){
+        this.head = null
+        this.size =0
+    }
+
+    isEmpty(){
+        return this.size == 0
+    }
+
+    getSize(){
+        return this.size
+    }
+
+    prepend(value){
+        const node = new Node(value)
+        if(this.isEmpty()){
+            this.head = node
+        }else{
+            node.next = this.head
+            this.head = node
+        }
+    this.size++
+    }
+}
+
+const list = new linkedList()
+console.log(list.isEmpty());
+console.log(list.getSize());
+list.prepend(10)
+list.prepend(20)
+list.prepend(30)
+console.log(list.isEmpty());
+console.log(list.getSize());
