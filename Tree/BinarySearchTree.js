f3148f0fcb57060d58ab2a620a4ea072171c9258
Ananthu Mohan <ananthumohan368@gmail.com>
2024-04-15 17:37:22 +0530

    binary search tree implementation

---
+++ Tree/BinarySearchTree.js
@@ -0,0 +1,50 @@
+class Node {
+    constructor(value) {
+        this.value = value
+        this.left = null
+        this.right = null
+    }
+}
+
+class Bst {
+    constructor() {
+        this.root = null
+    }
+
+    isEmpty() {
+        return this.root === null
+    }
+
+    insertion(value) {
+        const newNode = new Node(value)
+        if (this.isEmpty()) {
+            this.root = newNode
+        }else{
+            this.insertNode(this.root, newNode)
+        }
+    }
+
+    insertNode(root, newNode){
+        if(newNode.value < root.value){
+            if(root.left === null){
+                root.left = newNode
+            }else{
+                this.insertNode(root.left, newNode)
+            }
+        }else{
+            if(root.right === null){
+                root.right = newNode
+            }else{
+                this.insertNode(root.right, newNode)
+            }
+        }
+    }
+}
+
+
+
+const bst = new Bst()
+bst.insertion(50)
+bst.insertion(10)
+bst.insertion(60)
+console.log(bst.isEmpty());
