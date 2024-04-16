894143d22e3b0d17c64a360e7693a9616e17a63f
Ananthu Mohan <ananthumohan368@gmail.com>
2024-04-16 22:40:01 +0530

    binary search tree min max and deletion

--- Tree/BinarySearchTree.js
+++ Tree/BinarySearchTree.js
@@ -19,32 +19,123 @@ class Bst {
         const newNode = new Node(value)
         if (this.isEmpty()) {
             this.root = newNode
-        }else{
+        } else {
             this.insertNode(this.root, newNode)
         }
     }
 
-    insertNode(root, newNode){
-        if(newNode.value < root.value){
-            if(root.left === null){
+    insertNode(root, newNode) {
+        if (newNode.value < root.value) {
+            if (root.left === null) {
                 root.left = newNode
-            }else{
+            } else {
                 this.insertNode(root.left, newNode)
             }
-        }else{
-            if(root.right === null){
+        } else {
+            if (root.right === null) {
                 root.right = newNode
-            }else{
+            } else {
                 this.insertNode(root.right, newNode)
             }
         }
+    }
+
+
+    search(root, value) {
+        if(!root){
+            return false
+        }else{
+            if(root.value === value){
+                return `value ${value} is existed`
+            }else if(value < root.value){
+                return this.search(root.left, value)
+            }else{
+                return this.search(root.right, value)
+            }
+        }
+    }
+
+    preOrder(root){
+        if(root){
+            console.log(root.value);
+            this.preOrder(root.left)
+            this.preOrder(root.right)
+        }
+    }
+
+    inOrder(root){
+        if(root){
+            this.inOrder(root.left)
+            console.log(root.value);
+            this.inOrder(root.right)
+        }
+    }
+
+    postOrder(root){
+        if(root){
+            this.postOrder(root.left)
+            this.postOrder(root.right)
+            console.log(root.value);
+        }
+    }
+
+    min(root){
+        if(!root.left){
+            return root.value
+        }else{
+            return this.min(root.left)
+        }
+    }
+
+    max(root){
+        if(!root.right){
+            return root.value
+        }else{
+            return this.max(root.right)
+        }
+    }
+
+    delete(value){
+        this.root = this.deleteNode(this.root, value)
+    }
+
+    deleteNode(root, value){
+        if(root === null){
+            return root
+        }
+        if(value < root.value){
+            root.left = this.deleteNode(root.left, value)
+        }else if(value > root.value){
+            root.right = this.deleteNode(root.right, value)
+        } else {
+            if( !root.left && !root.right){
+                return null
+            }
+            if(!root.left){
+                return root.right
+            } else if(!root.right){
+                return root.left
+            }
+            root.value = this.min(root.right)
+            root.right = this.deleteNode(root.right, root.value)
+        }
+        return root
     }
 }
 
 
 
 const bst = new Bst()
-bst.insertion(50)
 bst.insertion(10)
-bst.insertion(60)
-console.log(bst.isEmpty());
+bst.insertion(5)
+bst.insertion(3)
+bst.insertion(7)
+bst.insertion(15)
+// console.log(bst.search(bst.root, 10));
+// bst.preOrder(bst.root)
+// bst.inOrder(bst.root)
+// bst.postOrder(bst.root)
+bst.delete(3)
+console.log(bst.min(bst.root));
+console.log(bst.max(bst.root));
+
